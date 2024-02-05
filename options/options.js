1583fbdb41421eafe168c39f802735ede7f151bb
Crowdbotics <team@crowdbotics.com>
2024-02-05 21:09:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smarterrand-45995",
+  url: "https://smarterrand-45995.botics.co",
+  api: "https://smarterrand-45995.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
